8f967f0108e1ed4da809bf9e50f73832a54cffa2
Daichi Kimura <dkimura@bdd.xyz>
2020-07-16 23:13:48 +0900

    chore: Fix standard

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,7 +1,14 @@
+const path = require('path')
+
 module.exports = {
-  "**/*.puml": (filenames) =>
-    filenames.map(
-      (filename) =>
-        `puml generate '${filename}'  --png --output dist/'${filename}'.png`
-    ),
-};
+  '*.puml': filenames => {
+    const cwd = process.cwd()
+
+    return filenames.map(
+      filename => {
+        const relativePath = path.relative(cwd, filename)
+        return `puml generate '${relativePath}'  --png --output dist/'${relativePath}'.png`
+      }
+    )
+  }
+}

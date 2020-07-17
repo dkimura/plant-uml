df531dc3e2429e092bbe89f2b4ae0d52e992e04d
Daichi Kimura <dkimura@bdd.xyz>
2020-07-17 12:11:11 +0900

    refactor: create dist dir

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,14 +1,21 @@
 const path = require('path')
+const fs = require("fs")
+
+const outputDir = path.resolve('dist')
 
 module.exports = {
   '*.puml': filenames => {
     const cwd = process.cwd()
 
+    if (!fs.existsSync(outputDir)) {
+      fs.mkdirSync(outputDir);
+    }
+
     return filenames.map(
       filename => {
         const relativePath = path.relative(cwd, filename)
         const baseName = path.basename(relativePath, '.puml')
-        return `puml generate ${relativePath} --png --output dist/${baseName}.png && git add`
+        return `puml generate ${relativePath} --png --output dist/${baseName}.png`
       }
     )
   }
